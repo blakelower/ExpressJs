770491e3bdf7f3c2460add3570201b06a98d000c
blakelower <blakelower1999@gmail.com>
2020-01-23 20:31:17 +0700

    update

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+
+
+//Logoin page
+router.get('/login', (req, res) => res.send('Login'))
+
+//Register Page
+router.get('/register', (req, res) => res.send('Register'))
+
+
+module.exports = router;
