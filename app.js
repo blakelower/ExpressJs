fc3ce23543f2226e8270db26af7226f5b039f752
blakelower <blakelower1999@gmail.com>
2020-01-23 21:33:05 +0700

    added

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//DB config
+const db = require("./config/keys").MongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 //EJS MIDDLEWARE
 app.use(expressLayouts);
-app.set('view engine', 'ejs')
-
+app.set("view engine", "ejs");
+//BodyParser
+app.use(express.urlencoded({extended: false}));
 //ROUTES
-app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on post ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://blakelower:blakelower8@cluster0-9slgh.mongodb.net/test?retryWrites=true&w=majority'
+}
